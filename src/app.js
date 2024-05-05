244deb5ab17c1bc70096bcb374168af8ffe77842
Dan Retegan <danretegan@yahoo.com>
2024-05-05 13:13:42 +0300

    Deleting Todos
    remove
    parentElement
    contains

--- src/app.js
+++ src/app.js
@@ -23,3 +23,12 @@ addForm.addEventListener('submit', e => {
     addForm.reset(); // Curățăm conținutul câmpului de input
   }
 });
+
+//TODO delete todos:
+// Adaugam addEventListener pe parintele inregistrarii TODO. Parintele unei inregistrari TODO este intreaga lista de TODO-uri. Astfel, datorita propagarii in sus a evenimentelor (bubbling), click-ul pe orice children, inclusiv pe un nou TODO adaugat, va fi capturat cu ajutorul acestui EventListener:
+list.addEventListener('click', e => {
+  // Verificăm dacă elementul pe care s-a făcut click conține clasa "delete", asociată cu iconița de ștergere (recycleBin). Dacă da, eliminam elementul parinte care, de data aceasta, este intregul element 'li' (inregistrarea TODO curenta):
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
