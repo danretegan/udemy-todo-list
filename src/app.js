37b5f8372624a0262715cf9aad36918b01e5caff
Dan Retegan <danretegan@yahoo.com>
2024-05-05 12:08:57 +0300

    Add a new todo

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+  const html = `
+  <li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  //   Nu permitem introducerea campurilor goale. generateTemplate() va fi apelat doar daca am introdus ceva ce are lungimea mai mare dacat 0:
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset(); // Curățăm conținutul câmpului de input
+  }
+});
