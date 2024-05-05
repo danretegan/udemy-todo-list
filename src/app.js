a73bf4d0b13feecd6036ca0df1083a35a94ea848
Dan Retegan <danretegan@yahoo.com>
2024-05-05 12:13:48 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@ const list = document.querySelector('.todos');
 
 const generateTemplate = todo => {
   const html = `
-  <li
-          class="list-group-item d-flex justify-content-between align-items-center"
-        >
-          <span>${todo}</span>
-          <i class="far fa-trash-alt delete"></i>
-        </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>
   `;
 
   list.innerHTML += html;
@@ -16,9 +14,10 @@ const generateTemplate = todo => {
 
 addForm.addEventListener('submit', e => {
   e.preventDefault();
+
   const todo = addForm.add.value.trim();
 
-  //   Nu permitem introducerea campurilor goale. generateTemplate() va fi apelat doar daca am introdus ceva ce are lungimea mai mare dacat 0:
+  // Nu permitem introducerea campurilor goale. generateTemplate() va fi apelat doar daca am introdus ceva ce are lungimea mai mare dacat 0:
   if (todo.length) {
     generateTemplate(todo);
     addForm.reset(); // Curățăm conținutul câmpului de input
