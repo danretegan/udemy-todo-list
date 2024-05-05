b062adc6dfa056dbfc3a940282f4c896b8cfcb70
Dan Retegan <danretegan@yahoo.com>
2024-05-05 14:31:10 +0300

    Searching & Filtering Todos

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const addForm = document.querySelector('.add');
+const search = document.querySelector('.search input');
 const list = document.querySelector('.todos');
 
 const generateTemplate = todo => {
   const html = `
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${todo}</span>
-    <i class="far fa-trash-alt delete"></i>
-  </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
   `;
 
   list.innerHTML += html;
 };
 
+const filterTodos = term => {
+  //? add filtered class:
+  Array.from(list.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+  //? remove filtered class:
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
+//TODO add todos:
 addForm.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -31,4 +45,10 @@ list.addEventListener('click', e => {
   if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
+});
+
+//TODO filter todos event:
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
